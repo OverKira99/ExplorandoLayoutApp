a0855a513cd94908b9b0440b85d17a549af53969
OverKira99 <lbeltran598@unab.edu.co>
2024-02-23 18:08:00 -0500

    final

--- app/src/main/java/com/alejandrobeltran/explorandolayoutapp/ProfileActivity.java
+++ app/src/main/java/com/alejandrobeltran/explorandolayoutapp/ProfileActivity.java
@@ -1,13 +1,23 @@
 package com.alejandrobeltran.explorandolayoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private Button btnMenuProf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        btnMenuProf = findViewById(R.id.btn_menuProf);
+    }
+    public void clickMenuTable(View view){
+        Intent miintent = new Intent(ProfileActivity.this , MainActivity.class);
+        startActivity(miintent);
     }
 }
