2d401b4919f0c806e7caf0504bf1432dd6765a27
OverKira99 <lbeltran598@unab.edu.co>
2024-02-23 17:23:11 -0500

    Constraint layout

--- app/src/main/java/com/alejandrobeltran/explorandolayoutapp/ConstraintActivity.java
+++ app/src/main/java/com/alejandrobeltran/explorandolayoutapp/ConstraintActivity.java
@@ -2,13 +2,23 @@ package com.alejandrobeltran.explorandolayoutapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConstraintActivity extends AppCompatActivity {
 
+    private Button btnMenu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_constraint);
+        btnMenu = findViewById(R.id.btn_menuCons);
     }
+    public void clickMenuCons(View view){
+        Intent miintent = new Intent(ConstraintActivity.this, MainActivity.class);
+        startActivity(miintent);
+    }
+
 }
